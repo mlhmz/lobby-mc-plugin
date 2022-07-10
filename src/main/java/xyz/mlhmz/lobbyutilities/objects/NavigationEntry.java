08312faa40ec4237392d44bcd14639e6fd61ab7b
Malek Hamza <malek.hamza@gmx.de>
2022-07-10 10:19:01 +0200

    Added lombok annotations to NavigationEntry

--- src/main/java/xyz/mlhmz/lobbyutilities/objects/NavigationEntry.java
+++ src/main/java/xyz/mlhmz/lobbyutilities/objects/NavigationEntry.java
@@ -1,51 +1,18 @@
 package xyz.mlhmz.lobbyutilities.objects;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.bukkit.Location;
 import org.bukkit.inventory.ItemStack;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class NavigationEntry {
     private String name;
     private ItemStack item;
     private Location location;
-
-    public NavigationEntry(String name, ItemStack item, Location location) {
-        this.name = name;
-        this.item = item;
-        this.location = location;
-    }
-
-    public NavigationEntry(String name, Location location) {
-        this.name = name;
-        this.location = location;
-    }
-
-    public NavigationEntry(String name) {
-        this.name = name;
-    }
-
-    public NavigationEntry() { }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public ItemStack getItem() {
-        return item;
-    }
-
-    public void setItem(ItemStack item) {
-        this.item = item;
-    }
-
-    public Location getLocation() {
-        return location;
-    }
-
-    public void setLocation(Location location) {
-        this.location = location;
-    }
 }
