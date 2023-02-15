a71e5aaa782c2e11e640cd330d3dfb877e86575f
Malek Hamza <malek.hamza@gmx.de>
2023-02-15 21:02:49 +0100

    Improve ChatEventListener.java

--- src/main/java/xyz/mlhmz/lobbyutilities/listeners/ChatEventListener.java
+++ src/main/java/xyz/mlhmz/lobbyutilities/listeners/ChatEventListener.java
@@ -15,27 +15,25 @@ public class ChatEventListener implements Listener {
 
     public ChatEventListener(LobbyUtilities plugin) {
         this.plugin = plugin;
-
         Bukkit.getServer().getPluginManager().registerEvents(this, plugin);
     }
 
     @EventHandler(priority = EventPriority.HIGH)
-    public static void onChat(AsyncPlayerChatEvent e) {
-        Player p = e.getPlayer();
-        String Message = e.getMessage();
-        e.setFormat(ChatUtils.translate("&e" + p.getName() + "&8: &7" + e.getMessage()));
+    public void onChat(AsyncPlayerChatEvent e) {
+        e.setFormat(ChatUtils.translate("&e%1$s&8: &7%2$s"));
     }
 
     @EventHandler(priority = EventPriority.LOW)
-    public static void onMention(AsyncPlayerChatEvent e) {
+    public void onMention(AsyncPlayerChatEvent e) {
         Player sender = e.getPlayer();
-        for (Player p : Bukkit.getOnlinePlayers()) {
+        for (Player p : plugin.getServer().getOnlinePlayers()) {
             if (e.getMessage().contains(p.getName())) {
-                String Message = e.getMessage();
-                e.setMessage(ChatUtils.translate(Message.replace(p.getName(), "&e@" + p.getName() + "&7" )));
+                String message = e.getMessage();
+                e.setMessage(ChatUtils.translate(message.replace(p.getName(), "&e@" + p.getName() + "&7" )));
                 p.playSound(p.getEyeLocation(), Sound.BLOCK_NOTE_BLOCK_BASS, 5, 1);
                 // One second has 20 ticks so everything is multiplied by 20
-                p.sendTitle(ChatUtils.translate("&ePaym &7hat dich erwähnt!"), ChatUtils.translate("&8Guck doch mal in den Chat."), 20, 20 * 3, 20);
+                p.sendTitle(ChatUtils.translate(String.format("&e%s &7hat dich erwähnt!", sender.getName())),
+                        ChatUtils.translate("&8Guck doch mal in den Chat."), 20, 20 * 3, 20);
             }
         }
     }
