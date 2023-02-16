08051a3b57406d6c0bc52890e55f52fbfbeeff89
Malek Hamza <malek.hamza@gmx.de>
2023-02-16 13:17:08 +0100

    Extend ModuleUtils with Module List Creation

--- src/main/java/xyz/mlhmz/lobbyutilities/util/ModuleUtils.java
+++ src/main/java/xyz/mlhmz/lobbyutilities/util/ModuleUtils.java
@@ -2,10 +2,15 @@ package xyz.mlhmz.lobbyutilities.util;
 
 import org.bukkit.event.Listener;
 import xyz.mlhmz.lobbyutilities.command.ExtendedCommand;
+import xyz.mlhmz.lobbyutilities.module.PluginModule;
 
 import java.util.List;
 
 public class ModuleUtils {
+    public static List<PluginModule> createModuleList(PluginModule... modules) {
+        return createObjectList(modules);
+    }
+
     public static List<ExtendedCommand> createCommandList(ExtendedCommand... commands) {
         return createObjectList(commands);
     }
